9f31c9a9a94d60115e9c8503efefad799648370d
Chen Stan <stan.chen@ssbshoes.com>
2021-12-31 16:30:20 +0700

    MonthReportDAO

--- MMS-API/Data/Interface/MMS/IMonthReportDAO.cs
+++ MMS-API/Data/Interface/MMS/IMonthReportDAO.cs
@@ -1,9 +1,10 @@
+using System.Collections.Generic;
 using API.Models.MMS;
 
 namespace API.Data.Interface.MMS
 {
     public interface IMonthReportDAO : ICommonDAO<MonthReport>
     {
-
+        public List<MonthReport> GetTop12Month();
     }
 }
--- MMS-API/Data/Repository/MMS/MonthReportDAO.cs
+++ MMS-API/Data/Repository/MMS/MonthReportDAO.cs
@@ -1,6 +1,9 @@
 
+using System.Collections.Generic;
+using System.Linq;
 using API.Data.Interface.MMS;
 using API.Models.MMS;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data.Repository.MMS
 {
@@ -10,5 +13,22 @@ namespace API.Data.Repository.MMS
         {
         }
 
+        public List<MonthReport> GetTop12Month()
+        {
+
+            string strSQL = string.Format(@";WITH top12 AS
+(
+   SELECT *,
+         ROW_NUMBER() OVER (PARTITION BY StockId ORDER BY YearMonth DESC) AS rn
+   FROM MonthReport
+)
+SELECT StockId,Revenue,YearMonth,YearQ,PreRevenue,UpdateTime
+FROM top12
+WHERE rn < 13");
+
+            var data = _context.MonthReport
+            .FromSqlRaw(strSQL).ToList();
+            return data;
+        }
     }
 }
